3ebdf18bb474793cc73ea9c14a76346e6b560899
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-12-21 11:25:34 +0000

    Layout Setup

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
