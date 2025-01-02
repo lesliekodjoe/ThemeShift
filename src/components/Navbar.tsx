ff2f5d5ea131532827828ca50fb56aad4b348b23
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2025-01-02 21:01:42 +0000

    Setup navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Logo from '../assets/Logo.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <nav className='px-5'>
+        <div className='flex justify-between items-center'>
+            <div>
+              <img src={Logo} className='w-20 h-20'/>
+            </div>
+            <div className='uppercase'>
+              menu
+            </div>
+        </div>
+    </nav>
+    </>
   )
 }
 
