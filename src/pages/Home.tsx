4c8e44f97d10992322d04ec22472fc316c77eb2e
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2025-03-24 22:07:30 +0000

    Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,9 @@ const Home = () => {
     <section>
       <div className="px-5">
         <div className="bg-[#000000] text-white rounded-xl py-28 px-4 flex items-center justify-center">
-          <div className="flex flex-col items-center justify-center gap-y-8 max-w-3xl border">
+          <div className="flex flex-col items-center justify-center gap-y-8 max-w-3xl">
             <h1 className="text-2xl text-center font-semibold">Discover Tech Stack, Endless UI Possibilites</h1>
-            <p className="text-center text-[14px] px-4 text-white/30">Explore different UI categories with different tech stack and elevate your project.</p>
+            <p className="text-center text-[14px] px-4 text-white/30 font-medium">Explore different UI categories with different tech stack and elevate your project.</p>
             <div className=" flex justify-between py-4 rounded-full bg-white bg-opacity-20 px-5 w-full items-center">
               <p className="text-[14px] text-opacity-45 text-white">Which UI do you need</p>
               <button className="text-sm">Search</button>
